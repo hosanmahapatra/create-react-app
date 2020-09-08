ed5b046b5339ce1b0a90d804f5498117a5d62e96
Hosan mohapatra <hosangolu7121@gmail.com>
2020-09-08 16:50:53 +0530

    design fix with ant library

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import "./App.css";
 import { GitCompare } from "./features/gitCompare/GitCompare";
+import "antd/dist/antd.css";
 
 const App: FC = () => {
   return (
--- src/features/gitCompare/GitCompare.tsx
+++ src/features/gitCompare/GitCompare.tsx
@@ -1,13 +1,27 @@
 import React, { FC } from "react";
-import { Input, Button, Row, Col } from "antd";
+import { Input, Button, Row, Col, Layout } from "antd";
+import styles from "./GitCompare.module.css";
 
 export const GitCompare: FC = () => {
   return (
-    <Row>
-      <Col span={24}>
-        <Input placeholder="Enter Git User Name" />
-        <Button type="primary">primary</Button>
-      </Col>
-    </Row>
+    <Layout>
+      <Layout.Header>&nbsp;</Layout.Header>
+      <Layout.Content className={styles.gitCompareContent}>
+        <Row>
+          <Col>&nbsp;</Col>
+        </Row>
+        <Row>
+          <Col span={8} offset={6}>
+            <Input placeholder="Enter Git User Name" />
+          </Col>
+          <Col span={8} style={{ textAlign: "left", paddingLeft: "15px" }}>
+            <Button type="primary">Compare</Button>
+          </Col>
+        </Row>
+      </Layout.Content>
+      <Layout.Footer style={{ textAlign: "center" }}>
+        Design ©2020 Created by Hosan Mohapatra
+      </Layout.Footer>
+    </Layout>
   );
 };
